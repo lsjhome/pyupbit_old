6d0494c1333895257ef311e741ebce0439880ca5
lsjhome007 <lsjhome007@gmail.com>
2018-11-02 20:14:28 +0900

    test_get_days_candles

--- test.py
+++ test.py
@@ -12,9 +12,16 @@ class PyUpBitTest(unittest.TestCase):
         self.assertNotEqual(len(ret), 0)
         logging.info(ret)
     
-    def test_get_minutes_data(self):
+    def test_get_minutes_candles(self):
         pyupbit = PyUpbit()
         ret = pyupbit.get_minutes_candles(60, 'KRW-BTC')
+        self.assertIsNotNone(ret)
+        self.assertNotEqual(len(ret), 0)
+        logging.info(ret)
+
+    def test_get_days_candles(self):
+        pyupbit = PyUpbit()
+        ret = pyupbit.get_days_candles('KRW-ADA')
         self.assertIsNotNone(ret)
         self.assertNotEqual(len(ret), 0)
         logging.info(ret)
